ebd7fde1c18453aa514ea4e6a970f3835f2b4d80
ArnaudDroxler <arnaud.droxler@gmail.com>
2017-11-09 13:58:07 +0100

    add some better filtering

--- js/grid.js
+++ js/grid.js
@@ -108,8 +108,11 @@ class Grid{
             this.gl.bindTexture(this.gl.TEXTURE_2D, this.normalTex);
             this.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, true);
             this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGB, this.gl.RGB, this.gl.UNSIGNED_BYTE, this.normalTex.image);
-            this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+            this.gl.generateMipmap(this.gl.TEXTURE_2D);
+            this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR_MIPMAP_LINEAR);
             this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
+            this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT); //Prevents s-coordinate wrapping (repeating).
+            this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT); //Prevents t-coordinate wrapping (repeating).
             this.gl.bindTexture(this.gl.TEXTURE_2D, null);
         }.bind(this);
         this.normalTex.image.src = this.normalTexPath;
